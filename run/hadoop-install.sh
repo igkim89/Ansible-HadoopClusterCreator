8418482d5803e2d5f6acd3a4d54513bec80c9da8
igkim89 <jel4399@gmail.com>
2022-12-09 16:59:04 +0900

    First commit.

---
+++ run/hadoop-install.sh
@@ -0,0 +1,1 @@
+~/.pyenv/shims/ansible-playbook -i /home/igkim/ansible/inventory/bigdata.yml /home/igkim/ansible/playbook/hadoop-install.yml
---
+++ run/zookeeper-install.sh
@@ -0,0 +1,1 @@
+~/.pyenv/shims/ansible-playbook -i /home/igkim/ansible/inventory/bigdata.yml -e '@/home/igkim/ansible/conf/bigdata.yml' /home/igkim/ansible/playbook/zookeeper-install.yml
