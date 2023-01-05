1ded1d86935cf36bc3073a964b8b6185217a87d3
igkim <jel4399@gmail.com>
2023-01-05 10:49:18 +0900

    Update pyspark-wordcount

---
+++ playbooks/test/templates/spark/pyspark-wordcount.py
@@ -0,0 +1,10 @@
+import datetime
+from pyspark import SparkContext
+
+sc = SparkContext()
+today_ts = str((datetime.datetime.now()).timestamp())
+text_file = sc.textFile("hdfs:///user/{{ remote_user }}/wordcount/input/sample01.txt")
+counts = text_file.flatMap(lambda line: line.split(" ")) \
+    .map(lambda word: (word, 1)) \
+    .reduceByKey(lambda a, b: a + b)
+counts.saveAsTextFile(f"hdfs:///user/{{ remote_user }}/wordcount/output/wordcount-spark-" + today_ts)
---
+++ run/igkim/test/pyspark-wordcount.sh
@@ -0,0 +1,1 @@
+~/.pyenv/shims/ansible-playbook -vv -i /home/igkim/ansible/environments/igkim/hosts.yml /home/igkim/ansible/playbooks/test/pyspark-wordcount.yml
