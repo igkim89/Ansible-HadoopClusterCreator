0305838a4e9033317f26cc28fda1a614f9fe2c7e
igkim <jel4399@gmail.com>
2023-01-09 16:12:55 +0900

    Update hive on spark

--- playbooks/test/templates/spark/pyspark-wordcount.py
+++ playbooks/test/templates/spark/pyspark-wordcount.py
@@ -1,17 +1,17 @@
-# import datetime
-# from pyspark import SparkContext
+import datetime
+from pyspark import SparkContext
+
+sc = SparkContext()
+today_ts = str((datetime.datetime.now()).timestamp())
+text_file = sc.textFile("hdfs:///user/{{ remote_user }}/wordcount/input/sample01.txt")
+counts = text_file.flatMap(lambda line: line.split(" ")) \
+    .map(lambda word: (word, 1)) \
+    .reduceByKey(lambda a, b: a + b)
+counts.saveAsTextFile(f"hdfs:///user/{{ remote_user }}/wordcount/output/pyspark-wordcount-" + today_ts)
+
+# from pyspark.sql.functions import *
 #
-# sc = SparkContext()
-# today_ts = str((datetime.datetime.now()).timestamp())
-# text_file = sc.textFile("hdfs:///user/{{ remote_user }}/wordcount/input/sample01.txt")
-# counts = text_file.flatMap(lambda line: line.split(" ")) \
-#     .map(lambda word: (word, 1)) \
-#     .reduceByKey(lambda a, b: a + b)
-# counts.saveAsTextFile(f"hdfs:///user/{{ remote_user }}/wordcount/output/pyspark-wordcount-" + today_ts)
-
-from pyspark.sql.functions import *
-
-textFile = spark.read.text("hdfs:///user/{{ remote_user }}/wordcount/input/sample01.txt")
-
-wordCounts = textFile.select(explode(split(textFile.value, "\s+")).alias("word")).groupBy("word").count()
-wordCounts.collect()
+# textFile = spark.read.text("hdfs:///user/{{ remote_user }}/wordcount/input/sample01.txt")
+#
+# wordCounts = textFile.select(explode(split(textFile.value, "\s+")).alias("word")).groupBy("word").count()
+# wordCounts.collect()
