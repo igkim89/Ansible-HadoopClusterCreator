05b890fa83770b224e808f32579a29ed1feaf788
igkim <jel4399@gmail.com>
2023-01-11 11:21:12 +0900

    Update hive on tez

--- run/dev/edit/edit-hive.sh
+++ run/dev/edit/edit-hive.sh
@@ -1,1 +1,3 @@
 ~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/dev/hosts.yml /home/bigdata/ansible/playbooks/edit/edit-hive.yml
+~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/dev/hosts.yml /home/bigdata/ansible/playbooks/edit/stop-hive.yml
+~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/dev/hosts.yml /home/bigdata/ansible/playbooks/edit/start-hive.yml
