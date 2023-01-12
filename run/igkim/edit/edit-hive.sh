fe5189be300c26f177e8c6ac367312cb09e5e882
igkim <jel4399@gmail.com>
2023-01-12 10:42:26 +0900

    Update Edit script

--- run/igkim/edit/edit-hive.sh
+++ run/igkim/edit/edit-hive.sh
@@ -1,3 +1,25 @@
+#!/bin/bash
+
 ~/.pyenv/shims/ansible-playbook -vv -i /home/igkim/ansible/environments/igkim/hosts.yml /home/igkim/ansible/playbooks/edit/edit-hive.yml
-~/.pyenv/shims/ansible-playbook -vv -i /home/igkim/ansible/environments/igkim/hosts.yml /home/igkim/ansible/playbooks/stop/stop-hive.yml
-~/.pyenv/shims/ansible-playbook -vv -i /home/igkim/ansible/environments/igkim/hosts.yml /home/igkim/ansible/playbooks/start/start-hive.yml
+
+echo -n "Do you want to restart the service now? (y/n) > "
+read restart
+
+while [ $read != "Y" ] && [ $read != "y" ] && [ $read != "N" ] && [ $read != "n" ]
+do
+  echo "Invalid input."
+  echo -n "Do you want to restart the service now? (y/n) > "
+  read restart
+done
+
+if [ $read = "Y" ] || [ $read = "y" ]
+then
+  ~/.pyenv/shims/ansible-playbook -vv -i /home/igkim/ansible/environments/igkim/hosts.yml /home/igkim/ansible/playbooks/stop/stop-hive.yml
+  ~/.pyenv/shims/ansible-playbook -vv -i /home/igkim/ansible/environments/igkim/hosts.yml /home/igkim/ansible/playbooks/start/start-hive.yml
+  echo "Service restarted."
+else
+  echo "Service was not restarted."
+  echo "A service restart is required to apply the changed settings."
+fi
+
+
