78c19d73298f8aa147551f284f16b6af92958437
igkim <jel4399@gmail.com>
2023-01-12 10:48:40 +0900

    Update Edit script

--- run/igkim/edit/edit-hive.sh
+++ run/igkim/edit/edit-hive.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+RED_COLOR='\033[0;31m'
+GREEN_COLOR='\033[0;32m'
+NO_COLOR='\033[0m'
+
 ~/.pyenv/shims/ansible-playbook -vv -i /home/igkim/ansible/environments/igkim/hosts.yml /home/igkim/ansible/playbooks/edit/edit-hive.yml
 
-echo -n "Do you want to restart the service now? (y/n) > "
+echo -n "${RED_COLOR}Do you want to restart the service now?${NO_COLOR} (y/n) > "
 read restart
 
 while [ $restart != "Y" ] && [ $restart != "y" ] && [ $restart != "N" ] && [ $restart != "n" ]
 do
   echo "Invalid input."
-  echo -n "Do you want to restart the service now? (y/n) > "
+  echo -n "${RED_COLOR}Do you want to restart the service now?${NO_COLOR} (y/n) > "
   read restart
 done
 
@@ -16,7 +20,7 @@ if [ $restart = "Y" ] || [ $restart = "y" ]
 then
   ~/.pyenv/shims/ansible-playbook -vv -i /home/igkim/ansible/environments/igkim/hosts.yml /home/igkim/ansible/playbooks/stop/stop-hive.yml
   ~/.pyenv/shims/ansible-playbook -vv -i /home/igkim/ansible/environments/igkim/hosts.yml /home/igkim/ansible/playbooks/start/start-hive.yml
-  echo "Service restarted."
+  echo "${GREEN_COLOR}Service restarted.${NO_COLOR}"
 else
   echo "Service was not restarted."
   echo "A service restart is required to apply the changed settings."
