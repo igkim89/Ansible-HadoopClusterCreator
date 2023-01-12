7f73c50e3a6cea7386c1b2bf7a6d76a40c683a94
igkim <jel4399@gmail.com>
2023-01-12 11:14:47 +0900

    Update Edit script

--- run/dev/edit/edit-hadoop.sh
+++ run/dev/edit/edit-hadoop.sh
@@ -1,3 +1,32 @@
+#!/bin/bash
+
+RED_COLOR='\033[0;31m'
+GREEN_COLOR='\033[0;32m'
+YELLOW_COLOR='\033[0;33m'
+NO_COLOR='\033[0m'
+
 ~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/dev/hosts.yml /home/bigdata/ansible/playbooks/edit/edit-hadoop.yml
-~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/dev/hosts.yml /home/bigdata/ansible/playbooks/stop/stop-hadoop.yml
-~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/dev/hosts.yml /home/bigdata/ansible/playbooks/start/start-hadoop.yml
+
+echo -n -e "${RED_COLOR}Do you want to restart the Hive service now?${NO_COLOR} (y/n) "
+read restart
+
+while [ $restart != "Y" ] && [ $restart != "y" ] && [ $restart != "N" ] && [ $restart != "n" ]
+do
+  echo "Invalid input."
+  echo -n -e "${RED_COLOR}Do you want to restart the Hive service now?${NO_COLOR} (y/n) "
+  read restart
+done
+
+if [ $restart = "Y" ] || [ $restart = "y" ]
+then
+  ~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/dev/hosts.yml /home/bigdata/ansible/playbooks/stop/stop-hadoop.yml
+  ~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/dev/hosts.yml /home/bigdata/ansible/playbooks/start/start-hadoop.yml
+else
+  echo -e "${YELLOW_COLOR}Hive service was not restarted.${NO_COLOR}"
+  echo -e "${YELLOW_COLOR}Hive service restart is required to apply the changed settings.${NO_COLOR}"
+fi
+
+
+
+
+
