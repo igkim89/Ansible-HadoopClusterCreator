1efabee8ecddcde7d83d4f1855f3ab863f47a259
igkim <jel4399@gmail.com>
2023-02-07 08:23:19 +0900

    Update HBase Editor

--- run/dev/edit/edit-hbase.sh
+++ run/dev/edit/edit-hbase.sh
@@ -5,7 +5,7 @@ GREEN_COLOR='\033[0;32m'
 YELLOW_COLOR='\033[0;33m'
 NO_COLOR='\033[0m'
 
-~/.pyenv/shims/ansible-playbook -vv -i /home/igkim/ansible/environments/igkim/hosts.yml /home/igkim/ansible/playbooks/edit/edit-hbase.yml
+~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/dev/hosts.yml /home/bigdata/ansible/playbooks/edit/edit-hbase.yml
 
 echo -n -e "${RED_COLOR}Do you want to restart the HBase service now?${NO_COLOR} (y/n) "
 read restart
@@ -19,12 +19,16 @@ done
 
 if [ $restart = "Y" ] || [ $restart = "y" ]
 then
-  ~/.pyenv/shims/ansible-playbook -vv -i /home/igkim/ansible/environments/igkim/hosts.yml /home/igkim/ansible/playbooks/stop/stop-hbase.yml
+  ~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/dev/hosts.yml /home/bigdata/ansible/playbooks/stop/stop-hbase.yml
   sleep 5s
-  ~/.pyenv/shims/ansible-playbook -vv -i /home/igkim/ansible/environments/igkim/hosts.yml /home/igkim/ansible/playbooks/start/start-hbase.yml
+  ~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/dev/hosts.yml /home/bigdata/ansible/playbooks/start/start-hbase.yml
 else
   echo -e "${YELLOW_COLOR}HBase service was not restarted.${NO_COLOR}"
   echo -e "${YELLOW_COLOR}HBase service restart is required to apply the changed settings.${NO_COLOR}"
 fi
+
+
+
+
 
 
