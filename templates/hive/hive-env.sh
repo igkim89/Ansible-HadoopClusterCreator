571a5d5028bfb3454207a56f60e9bbe0f9899d71
igkim <jel4399@gmail.com>
2023-03-03 17:23:27 +0900

    ADD Hadoop classpath in Hive

--- templates/hive/hive-env.sh
+++ templates/hive/hive-env.sh
@@ -1,3 +1,3 @@
 export SPARK_HOME={{ bigdata_home }}/spark
 TEZ_HOME={{ bigdata_home }}/tez
-export HADOOP_CLASSPATH=${TEZ_HOME}/conf:${TEZ_HOME}/*:${TEZ_HOME}/lib/*
+export HADOOP_CLASSPATH=$HADOOP_CLASSPATH:${TEZ_HOME}/conf:${TEZ_HOME}/*:${TEZ_HOME}/lib/*
