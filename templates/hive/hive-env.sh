4a517437cd71343359299f8fa94ed12de8f21949
igkim <jel4399@gmail.com>
2023-03-13 16:22:06 +0900

    Update R installer

--- templates/hive/hive-env.sh
+++ templates/hive/hive-env.sh
@@ -1,3 +1,2 @@
-export SPARK_HOME={{ bigdata_home }}/spark
-TEZ_HOME={{ bigdata_home }}/tez
+export SPARK_HOME={{ bigdata_home }}/{{ installer.hive_spark.replace('.tgz', '') }}
 export HADOOP_CLASSPATH=$HADOOP_CLASSPATH:${TEZ_HOME}/conf:${TEZ_HOME}/*:${TEZ_HOME}/lib/*
