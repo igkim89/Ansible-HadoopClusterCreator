e94d2fed611f31cb36d5f73f0e3000681788bb62
igkim <jel4399@gmail.com>
2023-06-05 15:32:00 +0900

    Update Prometheus reloader message

--- run/dev/edit/edit-prometheus.sh
+++ run/dev/edit/edit-prometheus.sh
@@ -7,10 +7,9 @@ NO_COLOR='\033[0m'
 
 ~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/dev/hosts.yml /home/bigdata/ansible/playbooks/edit/edit-prometheus.yml
 
-echo -n -e "${RED_COLOR}Do you want to restart the Prometheus service now?${NO_COLOR}"
-echo -n -e "${RED_COLOR}Alternatively, you can use the reload function to apply the changed settings.${NO_COLOR}"
-echo -n -e "${RED_COLOR}To use the 'reload' function without restarting, enter 'r' or 'R'${NO_COLOR}"
-echo -n -e "(y/n/r)"
+echo -n -e "${YELLOW_COLOR}You can use the reload function to apply the changed settings.${NO_COLOR} \n"
+echo -n -e "${YELLOW_COLOR}To use the 'reload' function without restarting, enter 'r' or 'R'${NO_COLOR} \n"
+echo -n -e "${RED_COLOR}Do you want to restart the Prometheus service now?${NO_COLOR} (y/n/r) "
 read restart
 
 while [ $restart != "Y" ] && [ $restart != "y" ] && [ $restart != "N" ] && [ $restart != "n" ] && [ $restart != "r" ] && [ $restart != "R" ]
@@ -28,7 +27,7 @@ then
 elif [ $restart = "r" ] || [ $restart = "R" ]
 then
   ~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/dev/hosts.yml /home/bigdata/ansible/playbooks/tool/prometheus/reload-config.yml
-  echo "Prometheus configuration reload was successful."
+  echo -n -e "${GREEN_COLOR}Prometheus configuration reload was successful.${NO_COLOR}"
 else
   echo -e "${YELLOW_COLOR}Prometheus service was not restarted.${NO_COLOR}"
   echo -e "${YELLOW_COLOR}Prometheus service restart is required to apply the changed settings.${NO_COLOR}"
