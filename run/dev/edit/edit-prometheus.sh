863fa079e16a79de2307c441beb8e9068bd1be30
igkim <jel4399@gmail.com>
2023-06-05 15:18:14 +0900

    Add Prometheus configruation reloader

--- run/dev/edit/edit-prometheus.sh
+++ run/dev/edit/edit-prometheus.sh
@@ -7,10 +7,13 @@ NO_COLOR='\033[0m'
 
 ~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/dev/hosts.yml /home/bigdata/ansible/playbooks/edit/edit-prometheus.yml
 
-echo -n -e "${RED_COLOR}Do you want to restart the Prometheus service now?${NO_COLOR} (y/n) "
+echo -n -e "${RED_COLOR}Do you want to restart the Prometheus service now?${NO_COLOR}"
+echo -n -e "${RED_COLOR}Alternatively, you can use the reload function to apply the changed settings.${NO_COLOR}"
+echo -n -e "${RED_COLOR}To use the 'reload' function without restarting, enter 'r' or 'R'${NO_COLOR}"
+echo -n -e "(y/n/r)"
 read restart
 
-while [ $restart != "Y" ] && [ $restart != "y" ] && [ $restart != "N" ] && [ $restart != "n" ]
+while [ $restart != "Y" ] && [ $restart != "y" ] && [ $restart != "N" ] && [ $restart != "n" ] && [ $restart != "r" ] && [ $restart != "R" ]
 do
   echo "Invalid input."
   echo -n -e "${RED_COLOR}Do you want to restart the Prometheus service now?${NO_COLOR} (y/n) "
@@ -22,6 +25,10 @@ then
   ~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/dev/hosts.yml /home/bigdata/ansible/playbooks/stop/stop-prometheus.yml
   sleep 5s
   ~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/dev/hosts.yml /home/bigdata/ansible/playbooks/start/start-prometheus.yml
+elif [ $restart = "r" ] || [ $restart = "R" ]
+then
+  ~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/dev/hosts.yml /home/bigdata/ansible/playbooks/tool/prometheus/reload-config.yml
+  echo "Prometheus configuration reload was successful."
 else
   echo -e "${YELLOW_COLOR}Prometheus service was not restarted.${NO_COLOR}"
   echo -e "${YELLOW_COLOR}Prometheus service restart is required to apply the changed settings.${NO_COLOR}"
