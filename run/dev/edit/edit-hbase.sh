022bf625d8d5632bf5de9baaccb6c2eac55ae5d5
igkim <jel4399@gmail.com>
2023-06-19 15:34:02 +0900

    Update HBase Rolling restart in editor

--- run/dev/edit/edit-hbase.sh
+++ run/dev/edit/edit-hbase.sh
@@ -7,13 +7,15 @@ NO_COLOR='\033[0m'
 
 ~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/dev/hosts.yml /home/bigdata/ansible/playbooks/edit/edit-hbase.yml
 
-echo -n -e "${RED_COLOR}Do you want to restart the HBase service now?${NO_COLOR} (y/n) "
+echo -e "${YELLOW_COLOR}You can use the Rolling restart.${NO_COLOR} \n"
+echo -e "${YELLOW_COLOR}Enter 'r' or 'R' to use HBase Master and RegionServer Rolling restart' function${NO_COLOR} \n"
+echo -n -e "${RED_COLOR}Do you want to restart the HBase service now?${NO_COLOR} (y/n/r) "
 read restart
 
-while [ $restart != "Y" ] && [ $restart != "y" ] && [ $restart != "N" ] && [ $restart != "n" ]
+while [ $restart != "Y" ] && [ $restart != "y" ] && [ $restart != "N" ] && [ $restart != "n" ] && [ $restart != "M" ] && [ $restart != "m" ] && [ $restart != "R" ] && [ $restart != "r" ]
 do
   echo "Invalid input."
-  echo -n -e "${RED_COLOR}Do you want to restart the HBase service now?${NO_COLOR} (y/n) "
+  echo -n -e "${RED_COLOR}Do you want to restart the HBase service now?${NO_COLOR} (y/n/r) "
   read restart
 done
 
@@ -22,6 +24,11 @@ then
   ~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/dev/hosts.yml /home/bigdata/ansible/playbooks/stop/stop-hbase.yml
   sleep 5s
   ~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/dev/hosts.yml /home/bigdata/ansible/playbooks/start/start-hbase.yml
+elif [ $restart = "R" ] || [ $restart = "r" ]
+then
+  ~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/dev/hosts.yml /home/bigdata/ansible/playbooks/tool/hbase/master-rolling-restart.yml
+  ~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/dev/hosts.yml /home/bigdata/ansible/playbooks/tool/hbase/region-rolling-restart.yml
+  echo -e "${GREEN_COLOR}HBase Rolling restart was successful.${NO_COLOR}"
 else
   echo -e "${YELLOW_COLOR}HBase service was not restarted.${NO_COLOR}"
   echo -e "${YELLOW_COLOR}HBase service restart is required to apply the changed settings.${NO_COLOR}"
