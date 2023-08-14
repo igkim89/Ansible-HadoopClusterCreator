78d086c3b24a50f65681da0cc6d3b0b7ba7b4806
igkim <jel4399@gmail.com>
2023-08-14 15:53:41 +0900

    Update cloud all installer

--- run/cloud/install/install-ALL.sh
+++ run/cloud/install/install-ALL.sh
@@ -1,3 +1,5 @@
 ~/.pyenv/shims/ansible-playbook -vv -i /home/cloud-user/ansible/environments/cloud/hosts.yml /home/cloud-user/ansible/playbooks/install/install-zookeeper.yml
 ~/.pyenv/shims/ansible-playbook -vv -i /home/cloud-user/ansible/environments/cloud/hosts.yml /home/cloud-user/ansible/playbooks/install/install-kafka.yml
+~/.pyenv/shims/ansible-playbook -vv -i /home/cloud-user/ansible/environments/cloud/hosts.yml /home/cloud-user/ansible/playbooks/install/install-hadoop.yml
+~/.pyenv/shims/ansible-playbook -vv -i /home/cloud-user/ansible/environments/cloud/hosts.yml /home/cloud-user/ansible/playbooks/install/install-mysql.yml
 
