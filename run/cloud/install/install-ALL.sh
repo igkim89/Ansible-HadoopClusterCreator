3f8c0806eca8526f675a88d1e571a0f29291a14f
igkim <jel4399@gmail.com>
2023-08-18 16:35:08 +0900

    Add Hue install script

--- run/cloud/install/install-ALL.sh
+++ run/cloud/install/install-ALL.sh
@@ -7,3 +7,4 @@
 ~/.pyenv/shims/ansible-playbook -vv -i /home/cloud-user/ansible/environments/cloud/hosts.yml /home/cloud-user/ansible/playbooks/install/install-hbase.yml
 ~/.pyenv/shims/ansible-playbook -vv -i /home/cloud-user/ansible/environments/cloud/hosts.yml /home/cloud-user/ansible/playbooks/install/install-spark.yml
 ~/.pyenv/shims/ansible-playbook -vv -i /home/cloud-user/ansible/environments/cloud/hosts.yml /home/cloud-user/ansible/playbooks/install/install-ufk.yml
+~/.pyenv/shims/ansible-playbook -vv -i /home/cloud-user/ansible/environments/cloud/hosts.yml /home/cloud-user/ansible/playbooks/install/install-hue.yml
