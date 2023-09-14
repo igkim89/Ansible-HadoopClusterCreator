1642835c04174a5d0a88133cf4023e6c8dfef44c
igkim <jel4399@gmail.com>
2023-09-14 17:58:47 +0900

    Update TODAY command

--- templates/mysql/mysql_dump.sh
+++ templates/mysql/mysql_dump.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-TODAY=$(date + '%Y%m%d')
+TODAY=$(date +'%Y%m%d')
 
 {{ mysql.home_dir }}/bin/mysqldump -uroot -p{{ mysql.root_pw }} --socket={{ mysql.socket_dir }}/mysql.sock --all-databases > /home/{{ remote_user }}/mysql_dump/mysql_dump_${TODAY}.sql
