bac31f500769e891e9054fe5853872ea20ba5a91
igkim <jel4399@gmail.com>
2024-02-29 10:13:21 +0900

    Update 'Spark host parameter'

--- run/cloud/stop/stop-spark.sh
+++ run/cloud/stop/stop-spark.sh
@@ -1,1 +1,16 @@
-~/.pyenv/shims/ansible-playbook -vv -i /home/cloud-user/ansible/environments/cloud/hosts.yml /home/cloud-user/ansible/playbooks/stop/stop-spark.yml
+#!/bin/bash
+
+if [ $# -gt 1 ]; then
+  echo "Usage: stop-spark.sh [<SERVER_FQDN>]"
+  echo "Example:"
+  echo "  stop-spark.sh"
+  echo "  stop-spark.sh datanode01.dev.bigdata"
+  echo "  stop-spark.sh datanode[01:05].dev.bigdata"
+  exit
+fi
+
+if [ $# -eq 0 ]; then
+  ~/.pyenv/shims/ansible-playbook -vv -i /home/cloud-user/ansible/environments/cloud/hosts.yml /home/cloud-user/ansible/playbooks/stop/stop-spark.yml
+else
+  ~/.pyenv/shims/ansible-playbook -vv -i /home/cloud-user/ansible/environments/cloud/hosts.yml /home/cloud-user/ansible/playbooks/stop/stop-spark.yml --extra-vars "variable_host=server-3.novalocal"
+fi
