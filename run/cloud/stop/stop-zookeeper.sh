77048744ffbf8d50e2a4ef2c80063e47127f634f
igkim <jel4399@gmail.com>
2024-03-04 08:54:18 +0900

    Update 'zookeeper service'

--- run/cloud/stop/stop-zookeeper.sh
+++ run/cloud/stop/stop-zookeeper.sh
@@ -2,18 +2,18 @@
 
 if [ $# -gt 1 ]; then
   echo ""
-  echo "Usage: stop-spark.sh [<SERVER_FQDN>]"
+  echo "Usage: stop-zookeeper.sh [<SERVER_FQDN>]"
   echo "Example:"
-  echo "  Default:     stop-spark.sh"
-  echo "  Single node: stop-spark.sh datanode01.dev.bigdata"
-  echo "  Multi node:  stop-spark.sh datanode[01:05:09].dev.bigdata"
-  echo "  Range node:  stop-spark.sh datanode[01-05].dev.bigdata"
+  echo "  Default:     stop-zookeeper.sh"
+  echo "  Single node: stop-zookeeper.sh datanode01.dev.bigdata"
+  echo "  Multi node:  stop-zookeeper.sh datanode[01:05:09].dev.bigdata"
+  echo "  Range node:  stop-zookeeper.sh datanode[01-05].dev.bigdata"
   echo ""
   exit
 fi
 
 if [ $# -eq 0 ]; then
-  ~/.pyenv/shims/ansible-playbook -vv -i /home/cloud-user/ansible/environments/cloud/hosts.yml /home/cloud-user/ansible/playbooks/stop/stop-spark.yml --extra-vars "variable_host=all"
+  ~/.pyenv/shims/ansible-playbook -vv -i /home/cloud-user/ansible/environments/cloud/hosts.yml /home/cloud-user/ansible/playbooks/stop/stop-zookeeper.yml --extra-vars "variable_host=all"
 else
-  ~/.pyenv/shims/ansible-playbook -vv -i /home/cloud-user/ansible/environments/cloud/hosts.yml /home/cloud-user/ansible/playbooks/stop/stop-spark.yml --extra-vars "variable_host=$1"
+  ~/.pyenv/shims/ansible-playbook -vv -i /home/cloud-user/ansible/environments/cloud/hosts.yml /home/cloud-user/ansible/playbooks/stop/stop-zookeeper.yml --extra-vars "variable_host=$1"
 fi
