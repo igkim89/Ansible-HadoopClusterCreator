f6e8d06a120d9b49750bb917e75c5208167a8f7b
igkim <jel4399@gmail.com>
2024-03-06 15:55:07 +0900

    Update 'Airflow Scheduler Stopper'

---
+++ run/cloud/stop/stop-airflow-scheduler.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $# -gt 1 ]; then
+  echo ""
+  echo "Usage: stop-airflow-scheduler.sh [<SERVER_FQDN>]"
+  echo "Example:"
+  echo "  Default:     stop-airflow-scheduler.sh"
+  echo "  Single node: stop-airflow-scheduler.sh datanode01.dev.bigdata"
+  echo "  Multi node:  stop-airflow-scheduler.sh datanode[01:05:09].dev.bigdata"
+  echo "  Range node:  stop-airflow-scheduler.sh datanode[01-05].dev.bigdata"
+  echo ""
+  exit
+fi
+
+if [ $# -eq 0 ]; then
+  ~/.pyenv/shims/ansible-playbook -vv -i /home/cloud-user/ansible/environments/cloud/hosts.yml /home/cloud-user/ansible/playbooks/stop/stop-airflow-scheduler.yml --extra-vars "variable_host=all"
+else
+  ~/.pyenv/shims/ansible-playbook -vv -i /home/cloud-user/ansible/environments/cloud/hosts.yml /home/cloud-user/ansible/playbooks/stop/stop-airflow-scheduler.yml --extra-vars "variable_host=$1"
+fi
