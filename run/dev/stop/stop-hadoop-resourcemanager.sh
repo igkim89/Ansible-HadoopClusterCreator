c5d53c4d1d3dea077571862a3f1d04489ea9cc93
igkim <jel4399@gmail.com>
2024-03-06 15:31:41 +0900

    Update 'ResourceManager Stopper'

---
+++ run/dev/stop/stop-hadoop-resourcemanager.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $# -gt 1 ]; then
+  echo ""
+  echo "Usage: stop-hadoop-resourcemanager.sh [<SERVER_FQDN>]"
+  echo "Example:"
+  echo "  Default:     stop-hadoop-resourcemanager.sh"
+  echo "  Single node: stop-hadoop-resourcemanager.sh datanode01.dev.bigdata"
+  echo "  Multi node:  stop-hadoop-resourcemanager.sh datanode[01:05:09].dev.bigdata"
+  echo "  Range node:  stop-hadoop-resourcemanager.sh datanode[01-05].dev.bigdata"
+  echo ""
+  exit
+fi
+
+if [ $# -eq 0 ]; then
+  ~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/dev/hosts.yml /home/bigdata/ansible/playbooks/stop/stop-hadoop-resourcemanager.yml --extra-vars "variable_host=all"
+else
+  ~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/dev/hosts.yml /home/bigdata/ansible/playbooks/stop/stop-hadoop-resourcemanager.yml --extra-vars "variable_host=$1"
+fi
