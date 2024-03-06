735638b4c92b0e0d212e14b43fb9eff1b5c88fd4
igkim <jel4399@gmail.com>
2024-03-06 15:45:22 +0900

    Update 'Hive Metastore Stopper'

---
+++ run/prod/stop/stop-hive-metastore.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $# -gt 1 ]; then
+  echo ""
+  echo "Usage: stop-hive-metastore.sh [<SERVER_FQDN>]"
+  echo "Example:"
+  echo "  Default:     stop-hive-metastore.sh"
+  echo "  Single node: stop-hive-metastore.sh datanode01.dev.bigdata"
+  echo "  Multi node:  stop-hive-metastore.sh datanode[01:05:09].dev.bigdata"
+  echo "  Range node:  stop-hive-metastore.sh datanode[01-05].dev.bigdata"
+  echo ""
+  exit
+fi
+
+if [ $# -eq 0 ]; then
+  ~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/prod/hosts.yml /home/bigdata/ansible/playbooks/stop/stop-hive-metastore.yml --extra-vars "variable_host=all"
+else
+  ~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/prod/hosts.yml /home/bigdata/ansible/playbooks/stop/stop-hive-metastore.yml --extra-vars "variable_host=$1"
+fi
