98a4fe268b9d417a6f294168295b1d53dde7d420
igkim <jel4399@gmail.com>
2024-03-11 10:19:18 +0900

    Update '--extra-vars'

--- run/prod/edit/edit-kafka.sh
+++ run/prod/edit/edit-kafka.sh
@@ -19,9 +19,9 @@ done
 
 if [ $restart = "Y" ] || [ $restart = "y" ]
 then
-  ~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/prod/hosts.yml /home/bigdata/ansible/playbooks/stop/stop-kafka.yml "variable_host=all"
+  ~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/prod/hosts.yml /home/bigdata/ansible/playbooks/stop/stop-kafka.yml --extra-vars "variable_host=all"
   sleep 5s
-  ~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/prod/hosts.yml /home/bigdata/ansible/playbooks/start/start-kafka.yml "variable_host=all"
+  ~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/prod/hosts.yml /home/bigdata/ansible/playbooks/start/start-kafka.yml --extra-vars "variable_host=all"
 else
   echo -e "${YELLOW_COLOR}Kafka service was not restarted.${NO_COLOR}"
   echo -e "${YELLOW_COLOR}Kafka service restart is required to apply the changed settings.${NO_COLOR}"
