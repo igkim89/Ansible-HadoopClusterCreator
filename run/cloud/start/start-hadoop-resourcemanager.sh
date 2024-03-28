5fdf8ead635674c47d1e6bb2c710ced74c66a462
igkim <jel4399@gmail.com>
2024-03-28 14:36:59 +0900

    Add 'Hadoop ResourceManager starter'

---
+++ run/cloud/start/start-hadoop-resourcemanager.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $# -gt 1 ]; then
+  echo ""
+  echo "Usage: start-hadoop-resourcemanager.sh [<SERVER_FQDN>]"
+  echo "Example:"
+  echo "  Default:     start-hadoop-resourcemanager.sh"
+  echo "  Single node: start-hadoop-resourcemanager.sh datanode01.dev.bigdata"
+  echo "  Multi node:  start-hadoop-resourcemanager.sh datanode[01:05:09].dev.bigdata"
+  echo "  Range node:  start-hadoop-resourcemanager.sh datanode[01-05].dev.bigdata"
+  echo ""
+  exit
+fi
+
+if [ $# -eq 0 ]; then
+  ~/.pyenv/shims/ansible-playbook -vv -i /home/cloud-user/ansible/environments/cloud/hosts.yml /home/cloud-user/ansible/playbooks/start/start-hadoop-resourcemanager.yml --extra-vars "variable_host=all"
+else
+  ~/.pyenv/shims/ansible-playbook -vv -i /home/cloud-user/ansible/environments/cloud/hosts.yml /home/cloud-user/ansible/playbooks/start/start-hadoop-resourcemanager.yml --extra-vars "variable_host=$1"
+fi
