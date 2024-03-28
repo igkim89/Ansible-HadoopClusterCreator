dcbf8247b53d305472774a49d9139cf5521ed69f
igkim <jel4399@gmail.com>
2024-03-28 17:03:24 +0900

    Add 'Airflow Webserver starter'

---
+++ run/dev/stop/stop-airflow-webserver.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $# -gt 1 ]; then
+  echo ""
+  echo "Usage: stop-airflow-webserver.sh [<SERVER_FQDN>]"
+  echo "Example:"
+  echo "  Default:     stop-airflow-webserver.sh"
+  echo "  Single node: stop-airflow-webserver.sh datanode01.dev.bigdata"
+  echo "  Multi node:  stop-airflow-webserver.sh datanode[01:05:09].dev.bigdata"
+  echo "  Range node:  stop-airflow-webserver.sh datanode[01-05].dev.bigdata"
+  echo ""
+  exit
+fi
+
+if [ $# -eq 0 ]; then
+  ~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/dev/hosts.yml /home/bigdata/ansible/playbooks/stop/stop-airflow-webserver.yml --extra-vars "variable_host=all"
+else
+  ~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/dev/hosts.yml /home/bigdata/ansible/playbooks/stop/stop-airflow-webserver.yml --extra-vars "variable_host=$1"
+fi
