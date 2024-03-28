1a7e3da03ba4e40504c737083ba0f59a3608d08c
igkim <jel4399@gmail.com>
2024-03-28 17:03:32 +0900

    Add 'Airflow Celery-Worker starter'

---
+++ run/cloud/stop/stop-airflow-celeryflower.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $# -gt 1 ]; then
+  echo ""
+  echo "Usage: stop-airflow-celeryflower.sh [<SERVER_FQDN>]"
+  echo "Example:"
+  echo "  Default:     stop-airflow-celeryflower.sh"
+  echo "  Single node: stop-airflow-celeryflower.sh datanode01.dev.bigdata"
+  echo "  Multi node:  stop-airflow-celeryflower.sh datanode[01:05:09].dev.bigdata"
+  echo "  Range node:  stop-airflow-celeryflower.sh datanode[01-05].dev.bigdata"
+  echo ""
+  exit
+fi
+
+if [ $# -eq 0 ]; then
+  ~/.pyenv/shims/ansible-playbook -vv -i /home/cloud-user/ansible/environments/cloud/hosts.yml /home/cloud-user/ansible/playbooks/stop/stop-airflow-celeryflower.yml --extra-vars "variable_host=all"
+else
+  ~/.pyenv/shims/ansible-playbook -vv -i /home/cloud-user/ansible/environments/cloud/hosts.yml /home/cloud-user/ansible/playbooks/stop/stop-airflow-celeryflower.yml --extra-vars "variable_host=$1"
+fi
