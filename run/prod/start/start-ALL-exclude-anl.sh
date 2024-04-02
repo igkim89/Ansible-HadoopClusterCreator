b86138105cb91f529ead181cc7eee692c1bfece8
igkim <jel4399@gmail.com>
2024-04-02 09:56:32 +0900

    Add 'All-Starter excluding analysis services'

--- run/prod/start/start-ALL-exclude-anl.sh
+++ run/prod/start/start-ALL-exclude-anl.sh
@@ -27,9 +27,9 @@ SCRIPT_LIST="
 
 for SCR in $SCRIPT_LIST ; do
   if [ $# -eq 0 ]; then
-    ~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/dev/hosts.yml /home/bigdata/ansible/playbooks/start/$SCR --extra-vars "variable_host=all"
+    ~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/prod/hosts.yml /home/bigdata/ansible/playbooks/start/$SCR --extra-vars "variable_host=all"
   else
-    ~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/dev/hosts.yml /home/bigdata/ansible/playbooks/start/$SCR --extra-vars "variable_host=$1"
+    ~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/prod/hosts.yml /home/bigdata/ansible/playbooks/start/$SCR --extra-vars "variable_host=$1"
   fi
 
   ret_val=$?
