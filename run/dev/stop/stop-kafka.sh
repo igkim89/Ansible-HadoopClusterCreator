c05c09e960eaf2b4ae7effcaab6f6c715d8903a2
igkim <jel4399@gmail.com>
2024-05-08 13:49:27 +0900

    Update 'Stop kafka script'

--- run/dev/stop/stop-kafka.sh
+++ run/dev/stop/stop-kafka.sh
@@ -2,18 +2,18 @@
 
 if [ $# -gt 1 ]; then
   echo ""
-  echo "Usage: stop-airflow.sh [<SERVER_FQDN>]"
+  echo "Usage: stop-kafka.sh [<SERVER_FQDN>]"
   echo "Example:"
-  echo "  Default:     stop-airflow.sh"
-  echo "  Single node: stop-airflow.sh datanode01.dev.bigdata"
-  echo "  Multi node:  stop-airflow.sh datanode[01:05:09].dev.bigdata"
-  echo "  Range node:  stop-airflow.sh datanode[01-05].dev.bigdata"
+  echo "  Default:     stop-kafka.sh"
+  echo "  Single node: stop-kafka.sh datanode01.dev.bigdata"
+  echo "  Multi node:  stop-kafka.sh datanode[01:05:09].dev.bigdata"
+  echo "  Range node:  stop-kafka.sh datanode[01-05].dev.bigdata"
   echo ""
   exit
 fi
 
 if [ $# -eq 0 ]; then
-  ~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/dev/hosts.yml /home/bigdata/ansible/playbooks/stop/stop-airflow.yml --extra-vars "variable_host=all"
+  ~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/dev/hosts.yml /home/bigdata/ansible/playbooks/stop/stop-kafka.yml --extra-vars "variable_host=all"
 else
-  ~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/dev/hosts.yml /home/bigdata/ansible/playbooks/stop/stop-airflow.yml --extra-vars "variable_host=$1"
+  ~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/dev/hosts.yml /home/bigdata/ansible/playbooks/stop/stop-kafka.yml --extra-vars "variable_host=$1"
 fi
