88eccfbc1aad3bdcb0d3aceeb58757aa3faf3192
igkim <jel4399@gmail.com>
2024-08-14 15:19:01 +0900

    Bug fix 'check_enable variables'

--- run/cloud/start/start-rangerPlugin.sh
+++ run/cloud/start/start-rangerPlugin.sh
@@ -8,14 +8,14 @@ NO_COLOR='\033[0m'
 echo -n -e "${RED_COLOR}Do you want enable Ranger plugins(HDFS, YARN, Hive, HBase)? ${NO_COLOR} (y/n) "
 read check_enable
 
-while [ check_enable != "Y" ] && [ check_enable != "y" ] && [ check_enable != "N" ] && [ check_enable != "n" ]
+while [ $check_enable != "Y" ] && [ $check_enable != "y" ] && [ $check_enable != "N" ] && [ $check_enable != "n" ]
 do
   echo "Invalid input."
   echo -n -e "${RED_COLOR}Do you want enable Ranger plugin?${NO_COLOR} (y/n) "
   read check_enable
 done
 
-if [ check_enable = "Y" ] || [ check_enable = "y" ]
+if [ $check_enable = "Y" ] || [ $check_enable = "y" ]
 then
   ~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/cloud/hosts.yml /home/bigdata/ansible/playbooks/start/start-rangerPlugin.yml --extra-vars "variable_host=all"
 else
--- run/dev/start/start-rangerPlugin.sh
+++ run/dev/start/start-rangerPlugin.sh
@@ -5,17 +5,17 @@ GREEN_COLOR='\033[0;32m'
 YELLOW_COLOR='\033[0;33m'
 NO_COLOR='\033[0m'
 
-echo -n -e "${RED_COLOR}Do you want enable Ranger plugins(HDFS, YARN, Hive, HBase)? ${NO_COLOR} (y/n) "
+echo -n -e "${RED_COLOR}Do you want enable Ranger plugins(HDFS, YARN, Hive, HBase)?${NO_COLOR} (y/n) "
 read check_enable
 
-while [ check_enable != "Y" ] && [ check_enable != "y" ] && [ check_enable != "N" ] && [ check_enable != "n" ]
+while [ $check_enable != "Y" ] && [ $check_enable != "y" ] && [ $check_enable != "N" ] && [ $check_enable != "n" ]
 do
   echo "Invalid input."
-  echo -n -e "${RED_COLOR}Do you want enable Ranger plugin?${NO_COLOR} (y/n) "
+  echo -n -e "${RED_COLOR}Do you want enable Ranger plugins(HDFS, YARN, Hive, HBase)?${NO_COLOR} (y/n) "
   read check_enable
 done
 
-if [ check_enable = "Y" ] || [ check_enable = "y" ]
+if [ $check_enable = "Y" ] || [ $check_enable = "y" ]
 then
   ~/.pyenv/shims/ansible-playbook -vv -i /home/bigdata/ansible/environments/dev/hosts.yml /home/bigdata/ansible/playbooks/start/start-rangerPlugin.yml --extra-vars "variable_host=all"
 else
